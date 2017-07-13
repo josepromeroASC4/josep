6e16e0fbd08508480ca47e9a51cfac6d69d93ab2
josepromeroASC4 <joeromero299@gmail.com>
2017-07-13 09:28:16 -0400

    updated ran

---
+++ random.js
@@ -0,0 +1,8 @@
+
+function rand(number){
+  var randomDecimalNumber = Math.random() * number;
+  var wholeIntegerNumber = Math.round(randomDecimalNumber);
+  return wholeIntegerNumber;
+}
+
+console.log(rand(6));
