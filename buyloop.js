a9c909ae0b57870da0c85b5dbbd8c97a22f13cd2
josepromeroASC4 <joeromero299@gmail.com>
2017-07-28 16:04:26 -0400

    pls work

---
+++ buyloop.js
@@ -0,0 +1,32 @@
+var prompt = require('prompt-sync')();
+var n = prompt('How many more times? ');
+var money = 1000;
+var louisVuittonBag = [];
+
+while(money >= 100){
+console.log("You have $" + money + "left")
+var answer = "You can buy yeezys for 400, gucci flip flops for 200, or ray bans for 100"}
+
+if(answer.toLowerCase() == "yeezys" && money >=500{
+
+    money = money - 400
+
+    louisVuittonBag.push
+    console.log("You have succesfully bought this pair of adidas Yeezys")
+}
+else if(answer == "Gucci Flip Flops"
+    money = money - 200;
+    louisVuittonBag.push("Gucci Flip flops")
+    console.log("You have successfully bought this pair of Gucci Flip Flops")
+}
+else if(answer === "Ray Bans"){
+    money = money - 100;
+    louisVuittonBag.push("Ray Bans")
+    console.log("You have successfully bought this pair of Ray Bans")
+}
+else{
+    console.log("You can't get this item");
+}
+
+}
+console.log("You broke b");
